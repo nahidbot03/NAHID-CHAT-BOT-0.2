e2b98fa3fa421a771b8e8fe2a820b0f2a489fc1d
nahidbot03 <cyberrihu32@gmail.com>
2025-10-20 09:50:23 +0600

    Update uptime.js

--- Script/commands/uptime.js
+++ Script/commands/uptime.js
@@ -48,8 +48,8 @@ module.exports = {
       const status = usedPercent < 70 ? "✅ SYSTEM STABLE" : usedPercent < 90 ? "⚠️ HIGH LOAD" : "⛔ CRITICAL";
       const finalMsg = `
 ╭───〔⚙️ SYSTEM STATUS ⚙️〕───╮
-│ 👑 𝗢𝗪𝗡𝗘𝗥: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
-│ 🤖 𝗕𝗢𝗧: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+│ 👑 𝗢𝗪𝗡𝗘𝗥: ♡𝐍𝐀𝐇𝐈𝐃♡
+│ 🤖 𝗕𝗢𝗧: ♡𝐍𝐀𝐇𝐈𝐃✦𝐂𝐇𝐀𝐓✦𝐁𝐎𝐓♡
 │ 🕐 𝗦𝗧𝗔𝗥𝗧 𝗧𝗜𝗠𝗘: ${startTime.toLocaleString()}
 │ ⏰ 𝗨𝗣𝗧𝗜𝗠𝗘: ${uptimeFormatted}
 ├───────────────────────
